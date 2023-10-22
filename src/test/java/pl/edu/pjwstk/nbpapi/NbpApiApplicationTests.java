92646bacbd996221970f37ef884348f050133049
blabladorrr <blabladorrr@gmail.com>
2023-10-22 19:18:25 +0200

    Initial commit

---
+++ src/test/java/pl/edu/pjwstk/nbpapi/NbpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjwstk.nbpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
